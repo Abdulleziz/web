8897d54107f71affb3162708c512089120f5a921
ilkergzlkkr <guzelkokarilker@gmail.com>
2023-03-20 16:32:07 +0300

    fix: dropdown align left

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,8 +28,8 @@ export const Navbar: React.FC = () => {
         </div>
       </div>
       <div className="navbar-center"></div>
-      <div className="navbar-end flex-none gap-2">
-        <div className="dropdown  ">
+      <div className="navbar-end">
+        <div className="dropdown dropdown-left">
           <label tabIndex={0} className="btn-ghost btn-circle avatar btn">
             <div className="w-10 rounded-full">
               {!!session?.user.image && <img src={session.user.image} />}
@@ -37,7 +37,7 @@ export const Navbar: React.FC = () => {
           </label>
           <ul
             tabIndex={0}
-            className="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 bg-gray-700 p-2 shadow"
+            className="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
           >
             <li>
               <a className="justify-between">
