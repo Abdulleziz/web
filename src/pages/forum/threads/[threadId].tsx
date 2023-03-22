67bac621c6819230667ffd9993a1f58bba640c0d
BoraVersioNNN <boraozdinc@hotmail.com>
2023-03-22 11:34:51 +0300

    delete on post and pages system added to thread

--- src/pages/forum/threads/[threadId].tsx
+++ src/pages/forum/threads/[threadId].tsx
@@ -3,6 +3,7 @@ import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { Layout } from "~/components/Layout";
+
 import {
   useCreateForumPost,
   useDeleteForumThread,
@@ -10,17 +11,21 @@ import {
 } from "~/utils/useForum";
 
 const ForumThread: NextPage = () => {
-  const router = useRouter();
   const [content, setContent] = useState("");
+  const router = useRouter();
   const { threadId } = router.query;
-
   const thread = useGetForumThread(threadId as string);
+
   const deleteThread = useDeleteForumThread();
   const createPost = useCreateForumPost();
 
+  const clearContent = () => {
+    setContent("");
+  };
+
   return (
     <Layout>
-      <div className="flex h-screen flex-col items-center justify-center p-4 py-2">
+      <div className="flex h-max flex-col items-center justify-center p-4 py-2">
         <div className="alert alert-info flex flex-row shadow-lg">
           <div>
             <InfoSVG />
@@ -57,34 +62,15 @@ const ForumThread: NextPage = () => {
                   {thread.data.createdAt.toLocaleString()}
                 </p>
               </div>
-              <div className="mt-3 flex flex-col  rounded bg-base-100 ">
-                {thread.data.posts.map((post) => (
-                  <div key={post.id} className="m-4 ml-2 flex flex-row">
-                    <div className="mr-4 rounded bg-base-200 ">
-                      {post.creator.image && (
-                        <img
-                          className="ml-auto mr-auto w-12 rounded-full p-1 sm:w-24"
-                          src={post.creator.image}
-                          alt="Profile Image"
-                        />
-                      )}
-                      <p className="m-3 text-center text-sm font-bold text-white sm:text-xl">
-                        {post.creator.name}
-                      </p>
-                    </div>
-                    <div className="flex w-full min-w-0 flex-1 rounded bg-base-200 ">
-                      <h3 className="m-8">{post.message}</h3>
-                    </div>
-                  </div>
-                ))}
-              </div>
+              <PostComponent />
 
               <div className="form-control mt-3">
                 <div className="input-group flex items-center justify-center">
                   <textarea
+                    name="messageInput"
                     className="input-bordered input w-full max-w-2xl"
-                    placeholder="Mesajınızı buraya yazın..."
-                    defaultValue={content}
+                    placeholder="Enter your message..."
+                    value={content}
                     onChange={(e) => setContent(e.target.value)}
                   />
                   <button
@@ -92,14 +78,15 @@ const ForumThread: NextPage = () => {
                       loading: createPost.isLoading,
                     })}
                     disabled={createPost.isLoading}
-                    onClick={() =>
+                    onClick={() => {
                       createPost.mutate({
                         threadId: threadId as string,
                         message: content,
-                      })
-                    }
+                      });
+                      clearContent();
+                    }}
                   >
-                    Gönder
+                    Send!
                   </button>
                 </div>
               </div>
@@ -130,6 +117,66 @@ const InfoSVG: React.FC = () => {
       ></path>
     </svg>
   );
+};
+
+const PostComponent: React.FC = () => {
+  const router = useRouter();
+  const { threadId } = router.query;
+  const thread = useGetForumThread(threadId as string);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  if (!thread?.data?.posts) {
+    return (
+      <Layout>
+        <div className="alert alert-error flex flex-row shadow-lg">
+          <InfoSVG />
+          <span>No posts found!</span>
+        </div>
+      </Layout>
+    );
+  } else {
+    const posts = thread.data.posts;
+    const postsPerPage = 5;
+    const startIndex = (currentPage - 1) * postsPerPage;
+    const endIndex = startIndex + postsPerPage;
+    const postsToDisplay = posts.slice(startIndex, endIndex);
+    const totalPages = Math.ceil(posts.length / postsPerPage);
+    return (
+      <div className="mt-3 flex flex-col  rounded bg-base-100 ">
+        {postsToDisplay &&
+          postsToDisplay.map((post) => (
+            <div key={post.id} className="m-4  flex flex-row">
+              <div className="mr-4 rounded bg-base-200 ">
+                {post.creator.image && (
+                  <img
+                    className="ml-auto mr-auto w-12 rounded-full p-1 sm:w-20"
+                    src={post.creator.image}
+                    alt="Profile Image"
+                  />
+                )}
+                <p className="m-3 text-center text-sm font-bold text-white sm:text-xl">
+                  {post.creator.name}
+                </p>
+              </div>
+              <div className="flex w-full min-w-0 flex-1 rounded bg-base-200 ">
+                <h3 className="m-8">{post.message}</h3>
+              </div>
+            </div>
+          ))}
+        <div className="mr-4 mb-4 flex flex-wrap justify-center space-y-0 space-x-2 md:justify-end md:space-y-0 md:space-x-4">
+          {Array.from({ length: totalPages }).map((_, i) => (
+            <button
+              key={i}
+              onClick={() => setCurrentPage(i + 1)}
+              className="rounded-md bg-blue-500 px-4 py-2 text-base transition-all hover:bg-blue-600 "
+            >
+              {i + 1}
+            </button>
+          ))}
+        </div>
+      </div>
+    );
+  }
 };
 
 export default ForumThread;
