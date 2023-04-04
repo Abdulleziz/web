a81eea010da53ae1d85a625c7d1518b921456724
BoraVersioNNN <boraozdinc@hotmail.com>
2023-04-04 15:05:35 +0300

    pin svg, pin button does not redirect anymore,

--- src/pages/forum/threads/index.tsx
+++ src/pages/forum/threads/index.tsx
@@ -40,11 +40,11 @@ const Threads: NextPage = () => {
             .sort((a, b) => sortByPin(a.pin, b.pin))
             .map((thread) => (
               <div key={thread.id}>
-                <li className="bg-base-200 transition-all hover:bg-base-300">
+                <li className=" relative bg-base-200 transition-all hover:bg-base-300">
                   <Link href={`/forum/threads/${thread.id}`}>
                     <div className="p-3">
                       {!!thread.pin && (
-                        <span>Pined by {thread.pin.pinnedBy.name}</span>
+                        <span>Pinned by {thread.pin.pinnedBy.name}</span>
                       )}
                       <h4 className="text-lg text-white">{thread.title}</h4>
                       <div className="flex flex-row items-center py-2">
@@ -57,38 +57,45 @@ const Threads: NextPage = () => {
                         )}
                         <p className="px-2">{thread.creator.name}</p>
                         <div className="ml-auto flex items-center justify-center gap-4 text-sm">
-                          {canPin && (
-                            <>
-                              {!thread.pin ? (
-                                <button
-                                  onClick={() => createPin.mutate(thread.id)}
-                                  className={classNames(
-                                    "btn-accent btn-sm btn",
-                                    { loading: createPin.isLoading }
-                                  )}
-                                >
-                                  Pinle!
-                                </button>
-                              ) : (
-                                <button
-                                  onClick={() => deletePin.mutate(thread.id)}
-                                  className={classNames(
-                                    "btn-error btn-sm btn",
-                                    { loading: deletePin.isLoading }
-                                  )}
-                                >
-                                  Pin{"'"}i Kaldır!
-                                </button>
-                              )}
-                            </>
-                          )}
                           <p>
-                            Oluşturulma: {thread.createdAt.toLocaleString()}
+                            Oluşturuldu: {thread.createdAt.toLocaleString()}
                           </p>
                         </div>
                       </div>
                     </div>
                   </Link>
+
+                  {canPin && (
+                    <div className="absolute top-3 right-3">
+                      {!thread.pin ? (
+                        <button
+                          onClick={() => createPin.mutate(thread.id)}
+                          className={classNames(
+                            "btn-accent btn-sm btn rounded-full",
+                            {
+                              loading: createPin.isLoading,
+                              disabled: createPin.isLoading,
+                            }
+                          )}
+                        >
+                          <PinSVG />
+                        </button>
+                      ) : (
+                        <button
+                          onClick={() => deletePin.mutate(thread.id)}
+                          className={classNames(
+                            "btn-error btn-sm btn rounded-full",
+                            {
+                              loading: deletePin.isLoading,
+                              disabled: createPin.isLoading,
+                            }
+                          )}
+                        >
+                          <UnpinSVG />
+                        </button>
+                      )}
+                    </div>
+                  )}
                 </li>
                 <div className="divider m-0 bg-base-200" />
               </div>
@@ -96,6 +103,48 @@ const Threads: NextPage = () => {
         </ul>
       )}
     </div>
+  );
+};
+
+export const PinSVG: React.FC = () => {
+  return (
+    <>
+      <svg
+        width="30px"
+        height="30px"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M14.579 14.579L11.6316 17.5264L10.7683 16.6631C10.3775 16.2723 10.1579 15.7422 10.1579 15.1894V13.1053L7.21052 10.158L5 9.42111L9.42111 5L10.158 7.21052L13.1053 10.1579L15.1894 10.1579C15.7422 10.1579 16.2722 10.3775 16.6631 10.7683L17.5264 11.6316L14.579 14.579ZM14.579 14.579L19 19"
+          stroke="#464455"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </>
+  );
+};
+
+export const UnpinSVG: React.FC = () => {
+  return (
+    <>
+      <svg
+        width="30px"
+        height="30px"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M14.579 14.579L11.6316 17.5264L11.0526 16.9474M14.579 14.579L17.5264 11.6316L16.9474 11.0526M14.579 14.579L19 19M5 19L10.1579 13.8421M19 5L13.8421 10.1579M13.8421 10.1579L13.1053 10.1579L10.158 7.21052L9.42111 5L5 9.42111L7.21052 10.158L10.1579 13.1053V13.8421M13.8421 10.1579L10.1579 13.8421"
+          stroke="#464455"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </>
   );
 };
 
