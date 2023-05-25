961bc8d5721ae9ad341313a6bcd485078bc9b60e
ilkergzlkkr <guzelkokarilker@gmail.com>
2023-05-25 14:56:00 +0300

    chore: useGetUserIdsByDiscordIds

--- src/utils/useProfile.ts
+++ src/utils/useProfile.ts
@@ -6,3 +6,7 @@ export const useGetProfile = (query: In["getProfileById"]) =>
   api.profile.getProfileById.useQuery(query);
 
 export const useGetMe = () => api.profile["@me"].useQuery();
+
+export const useGetUserIdsByDiscordIds = (
+  query: In["getUserIdsByDiscordIds"]
+) => api.profile.getUserIdsByDiscordIds.useQuery(query);
