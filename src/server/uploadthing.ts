e312f6725d7986fb0820b08b927b302ec645fba3
ilkergzlkkr <guzelkokarilker@gmail.com>
2023-06-10 01:26:01 +0300

    chore(version): bump uploadthing

--- src/server/uploadthing.ts
+++ src/server/uploadthing.ts
@@ -9,7 +9,6 @@ export const uploadRouter = {
   })
     .middleware(async (req, res) => {
       const ctx = await createTRPCContext({ req, res });
-      console.log("cookies for post upload attachment", req.cookies);
 
       if (!ctx.session) throw new Error("Unauthorized");
 
